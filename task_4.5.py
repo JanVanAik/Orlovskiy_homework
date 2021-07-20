94528f26f31069e105c5a3995a5095a9c064bed4
JanVanAik <ar.orlovskiy@gmail.com>
2021-07-21 02:41:40 +0300

    Lesson 3, task 3 and 5 completed

---
+++ task_4.5.py
@@ -0,0 +1,20 @@
+from sys import argv
+from requests import get, utils
+
+response = get('http://www.cbr.ru/scripts/XML_daily.asp')
+encodings = utils.get_encoding_from_headers(response.headers)
+content = response.content.decode(encoding=encodings)
+content = content.split('</Valute>')
+
+
+def currency_rates(money_code):
+    current_date = content[0].split('Date="')[1][:10]
+    for i in content:
+        if i.find(money_code.upper()) != -1:
+            i = i.split('<Value>')
+            return f"Курс к рублю: {float(i[1].replace('</Value>', '').replace(',','.'))} к 1 \n {current_date}"
+
+
+if __name__ == "__main__":
+    name, arg_1 = argv
+    print(currency_rates(arg_1))
